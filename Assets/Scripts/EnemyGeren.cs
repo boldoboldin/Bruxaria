a94bcbd1cfb521df2636a4db27091f02e24278cb
Luiz Guilherme de Souza Gonçalves <125094466+BabuMendes@users.noreply.github.com>
2023-09-20 20:48:56 -0300

    EnemyGeren

--- Assets/Scripts/EnemyGeren.cs
+++ Assets/Scripts/EnemyGeren.cs
@@ -6,19 +6,37 @@ public class EnemyGeren : MonoBehaviour
 {
     [SerializeField] private List<GameObject> enemies;
 
-    [SerializeField] private float shotSpeed;
-    [SerializeField] private float maxShotTime;
-    private float currentShotTime;
+    [SerializeField] private float mimSpawTime;
+    [SerializeField] private float maxSpawTime;
+    [SerializeField] private float currentSpawTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSpawTime = Random.Range(mimSpawTime, maxSpawTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemySpaw();
+    }
+
+    void EnemySpaw()
+    {
+        currentSpawTime -= Time.deltaTime;
+
+        if (currentSpawTime <= 0)
+        {
+            currentSpawTime = Random.Range(mimSpawTime, maxSpawTime);
+
+            float posY = Random.Range(-3.5f, 3.5f);
+            Vector3 spawPos = new Vector2(10, posY);
+
+            int enemyType = Random.Range(0, 1);
+
+            GameObject enemy = Instantiate(enemies[enemyType], spawPos, Quaternion.identity);
+            Destroy(enemy, 5f);
+        }
     }
 }

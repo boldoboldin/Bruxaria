7fcdcc116f2aa7bec4e30cc6cc6cd8d8c3047ef1
Henrique Baldo Carlos <80064286+boldoboldin@users.noreply.github.com>
2023-09-20 16:49:16 -0300

    Initial commit

---
+++ Assets/Scripts/EnemyCtrl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCtrl : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [Header("Status Variables")]
+    [SerializeField] private float hp;
+    [SerializeField] private float vel;
+    [SerializeField] private string status = "null";
+
+    [SerializeField] protected GameObject explosionFX;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(-vel, 0f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void EnemyHit(int damage)
+    {
+        hp = -damage;
+
+        if (hp <= 0f)
+        {
+            Instantiate(explosionFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [Header("Status Variables")]
+    [SerializeField] private float hp;
+    [SerializeField] private float def;
+    [SerializeField] private float vel;
+    [SerializeField] private string status = "null";
+
+    [Header("Shot Variables")]
+    [SerializeField] private GameObject playerShot;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float shotSpeed;
+    [SerializeField] private float maxShotTime;
+    private float currentShotTime;
+    public string shotType = "null";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        currentShotTime = maxShotTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shot();
+    }
+
+    private void Movement()
+    {
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 playerVel = new Vector2(0, moveY);
+
+        rb2D.velocity = playerVel * vel;
+    }
+
+    private void Shot()
+    {
+        currentShotTime -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space) && currentShotTime <= 0 && shotType != "null")
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        if (shotType == "Fire Ball")
+        {
+            Instantiate(playerShot, firePoint.transform.position, Quaternion.identity);
+        }
+        
+        currentShotTime = maxShotTime;
+    }
+
+    public void PlayerHit(int damage)
+    {
+        hp = hp - damage;
+
+        if (hp <= 0)
+        {
+            //Instantiate(explosionFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
